13da7ccc7a6376f41b5afcd38aace15806df78cb
2O4 <35725720+2O4@users.noreply.github.com>
2021-02-09 22:10:12 +0100

    test travis

--- dhost/host/tests/views.py
+++ dhost/host/tests/views.py
@@ -12,3 +12,43 @@ class SiteListViewTest(TestCase):
         context = view.get_context_data()
         self.assertIn("site_list", context)
 
+
+class SiteDetailViewTest(TestCase):
+    def test_environment_set_in_context(self):
+        request = RequestFactory().get("/host/")
+        view = SiteDetailView()
+        view.setup(request)
+
+        context = view.get_context_data()
+        self.assertIn("site_list", context)
+
+
+class SiteCreateViewTest(TestCase):
+    def test_environment_set_in_context(self):
+        request = RequestFactory().get("/host/")
+        view = SiteCreateView()
+        view.setup(request)
+
+        context = view.get_context_data()
+        self.assertIn("site_list", context)
+
+
+class SiteUpdateViewTest(TestCase):
+    def test_environment_set_in_context(self):
+        request = RequestFactory().get("/host/")
+        view = SiteUpdateView()
+        view.setup(request)
+
+        context = view.get_context_data()
+        self.assertIn("site_list", context)
+
+
+class SiteDeleteViewTest(TestCase):
+    def test_environment_set_in_context(self):
+        request = RequestFactory().get("/host/")
+        view = SiteDeleteView()
+        view.setup(request)
+
+        context = view.get_context_data()
+        self.assertIn("site_list", context)
+
