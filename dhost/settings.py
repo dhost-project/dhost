8cb217c94144cd1beb53cc75817880fc81a558f9
2O4 <35725720+2O4@users.noreply.github.com>
2021-02-14 13:52:07 +0100

    test Heroku staticfiles serving

--- dhost/settings.py
+++ dhost/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = [BASE_DIR / "dhost/static"]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+#STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
