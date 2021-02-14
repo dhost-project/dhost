3b633cfe3063a7195761f5a9fcaab332d58286f4
2O4 <35725720+2O4@users.noreply.github.com>
2021-02-14 13:59:00 +0100

    test collectstatic heroku

--- dhost/settings.py
+++ dhost/settings.py
@@ -92,9 +92,9 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# STATIC_URL = "/static/"
-# STATIC_ROOT = BASE_DIR / "static"
-# STATICFILES_DIRS = [BASE_DIR / "dhost/static"]
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = [BASE_DIR / "dhost/static"]
 # STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
