df2b47895cdf3e4e2faf69ba0cf50d83c808e7d5
2O4 <35725720+2O4@users.noreply.github.com>
2021-02-18 20:31:42 +0100

    added tailwind and awesomefont

--- dhost/urls.py
+++ dhost/urls.py
@@ -11,6 +11,14 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
 ]
 
+if settings.DEBUG:
+    from django.views.defaults import page_not_found
+
+    def page_not_found_debug(request):
+        return page_not_found(request, exception=None)
+
+    urlpatterns += [path("404", page_not_found_debug)]
+
 if settings.DEBUG_TOOLBAR:
     import debug_toolbar
 
