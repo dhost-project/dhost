1e2474782dc31287af43d965fb9b5c7436e6d2f6
2O4 <35725720+2O4@users.noreply.github.com>
2021-03-17 15:08:56 +0100

    format: yapf, github workflows

--- dhost/core/management/commands/deltestdir.py
+++ dhost/core/management/commands/deltestdir.py
@@ -9,7 +9,10 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--noinput', '--no-input', action='store_false', dest='interactive',
+            '--noinput',
+            '--no-input',
+            action='store_false',
+            dest='interactive',
             help='Tells Django to NOT prompt the user for input of any kind.',
         )
 
@@ -17,11 +20,13 @@ class Command(BaseCommand):
         interactive = options['interactive']
 
         if interactive:
-            confirm = input("""You have requested to remove the test folder.
+            confirm = input(
+                """You have requested to remove the test folder.
 This will IRREVERSIBLY DESTROY all data currently in the "%s" folder.
 Are you sure you want to do this?
 
-    Type 'yes' to continue, or 'no' to cancel: """ % settings.TEST_DIR)
+    Type 'yes' to continue, or 'no' to cancel: """ % settings.TEST_DIR
+            )
         else:
             confirm = 'yes'
 
