60d0cd9444d1b70dc308b5bdf2e6c0311e2693b6
2O4 <35725720+2O4@users.noreply.github.com>
2021-03-17 16:30:20 +0100

    feat: debug URLs

--- dhost/urls.py
+++ dhost/urls.py
@@ -13,3 +13,27 @@ if settings.ENABLE_DEBUG_TOOLBAR:
     import debug_toolbar
 
     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.views import defaults as default_views
+
+    urlpatterns += [
+        path(
+            "400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            "403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            "404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path("500/", default_views.server_error),
+    ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
