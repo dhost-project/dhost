ab74b9cb3090e03795385504585331ff5ed8f490
2O4 <35725720+2O4@users.noreply.github.com>
2021-05-18 08:18:26 +0200

    feat: dashboard logs app basic

---
+++ dhost/logs/models.py
@@ -0,0 +1,56 @@
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
+from dhost.dapps.models import Dapp
+
+ADDITION = 1
+CHANGE = 2
+DELETION = 3
+START_BUILD = 4
+BUILD_SUCCESS = 5
+BUILD_FAIL = 6
+START_DEPLOY = 7
+DEPLOY_SUCCESS = 8
+DEPLOY_FAIL = 9
+
+ACTION_FLAG_CHOICES = (
+    (ADDITION, _('Addition')),
+    (CHANGE, _('Change')),
+    (DELETION, _('Deletion')),
+    (START_BUILD, _('Start build')),
+    (BUILD_SUCCESS, _('Build successful')),
+    (BUILD_FAIL, _('Build failed')),
+    (START_DEPLOY, _('Start deployment')),
+    (DEPLOY_SUCCESS, _('Deployment successful')),
+    (DEPLOY_FAIL, _('Deployment failed')),
+)
+
+
+class DashboardLogEntry(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    dapp = models.ForeignKey(Dapp, on_delete=models.CASCADE)
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    object_id = models.TextField(null=True, blank=True)
+    action_flag = models.PositiveSmallIntegerField(
+        choices=ACTION_FLAG_CHOICES,
+        null=True,
+        blank=True,
+    )
+    change_message = models.TextField(blank=True)
+    action_time = models.DateTimeField(default=timezone.now, editable=False)
+
+    class Meta:
+        verbose_name = _('dashboard log entry')
+        verbose_name_plural = _('dashboard log entries')
+        ordering = ['-action_time']
+
+    def __str__(self):
+        return self.change_message
