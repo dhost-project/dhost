091ed62db7add7eed1d7f0285e4eea7f4c9e3711
2O4 <35725720+2O4@users.noreply.github.com>
2021-05-20 17:32:11 +0200

    feat: api url rename to v1, move builds views to dapps views

--- dhost/dapps/views.py
+++ dhost/dapps/views.py
@@ -1,16 +1,23 @@
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from dhost.builds.views import BuildOptionsViewSet
+from dhost.builds.views import (BuildOptionsViewSet, BuildsViewSet,
+                                BundleViewSet, EnvironmentVariableViewSet)
 
 from .models import Dapp
 from .serializers import AbstractDeploymentSerializer, DappSerializer
 
 
 class DappViewSet(BuildOptionsViewSet):
-    queryset = Dapp.objects.all()
     serializer_class = DappSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter user's apps"""
+        user = self.request.user
+        return Dapp.objects.filter(owner=user)
 
     @action(detail=True, methods=['get'])
     def deploy(self, request, pk=None):
@@ -24,3 +31,15 @@ class DappViewSet(BuildOptionsViewSet):
 
 class AbstractDeploymentViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = AbstractDeploymentSerializer
+
+
+class DappBundleViewSet(BundleViewSet):
+    pass
+
+
+class DappBuildsViewSet(BuildsViewSet):
+    pass
+
+
+class DappEnvironmentVariableViewSet(EnvironmentVariableViewSet):
+    pass
