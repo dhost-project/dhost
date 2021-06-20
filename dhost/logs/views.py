3a0779a47827f58478af55fff37d33710e5ae75d
2O4 <35725720+2O4@users.noreply.github.com>
2021-06-20 19:15:16 +0200

    add APILogViewSetMixin

--- dhost/logs/views.py
+++ dhost/logs/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 
+from dhost.dapps.views import DappViewMixin
+
 from .models import APILog
 from .serializers import APILogSerializer
 
 
-class APILogViewSet(viewsets.ReadOnlyModelViewSet):
+class APILogViewSet(DappViewMixin, viewsets.ReadOnlyModelViewSet):
     queryset = APILog.objects.all()
     serializer_class = APILogSerializer
