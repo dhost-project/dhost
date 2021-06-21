dcb5f29fd19cc9af9e6e3aeedfcc907e5ab7c6d2
2O4 <35725720+2O4@users.noreply.github.com>
2021-06-21 08:24:38 +0200

    logs model detection

--- dhost/logs/views_mixins.py
+++ dhost/logs/views_mixins.py
@@ -2,6 +2,11 @@ from django.contrib.contenttypes.models import ContentType
 
 from .models import APILog
 
+# The list of name of inherited `Dapp` models
+DAPP_MODELS_LIST = ['ipfsdapp']
+ENV_VAR_MODEL_LIST = ['envvar']
+BUILD_OPTIONS_MODEL_LIST = ['buildoptions']
+
 
 class APILogViewSetMixin:
     """
@@ -17,12 +22,21 @@ class APILogViewSetMixin:
     partial_update_action_flag = None
     log_on_destroy = True
     destroy_action_flag = None
-    # True if the current ViewSet is for a Dapp object
-    object_is_dapp = False
 
-    def get_create_action_flag(self):
+    def get_create_action_flag(self, obj=None):
         if self.create_action_flag:
             return self.create_action_flag
+        if obj:
+            obj_model = self.get_obj_model(obj).model
+            if obj_model in DAPP_MODELS_LIST:
+                from .models import DAPP_ADDITION
+                return DAPP_ADDITION
+            elif obj_model in ENV_VAR_MODEL_LIST:
+                from .models import ENV_VAR_ADDITION
+                return ENV_VAR_ADDITION
+            elif obj_model in BUILD_OPTIONS_MODEL_LIST:
+                from .models import BUILD_OPTIONS_ADDITION
+                return BUILD_OPTIONS_ADDITION
         raise Exception('Please provide a `create_action_flag`.')
 
     def perform_create(self, serializer):
@@ -32,16 +46,28 @@ class APILogViewSetMixin:
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
         if self.log_on_create:
+            obj = self.obj
+            action_flag = self.get_create_action_flag(obj)
             self.log_action(
                 request,
-                obj=self.obj,
-                action_flag=self.get_create_action_flag(),
+                obj=obj,
+                action_flag=action_flag,
             )
         return response
 
     def get_update_action_flag(self):
         if self.update_action_flag:
             return self.update_action_flag
+        obj_model = self.get_obj_model(self.get_object()).model
+        if obj_model in DAPP_MODELS_LIST:
+            from .models import DAPP_CHANGE
+            return DAPP_CHANGE
+        elif obj_model in ENV_VAR_MODEL_LIST:
+            from .models import ENV_VAR_CHANGE
+            return ENV_VAR_CHANGE
+        elif obj_model in BUILD_OPTIONS_MODEL_LIST:
+            from .models import BUILD_OPTIONS_CHANGE
+            return BUILD_OPTIONS_CHANGE
         raise Exception('Please provide an `update_action_flag`.')
 
     def update(self, request, *args, **kwargs):
@@ -57,10 +83,11 @@ class APILogViewSetMixin:
     def get_partial_update_action_flag(self):
         if self.partial_update_action_flag:
             return self.partial_update_action_flag
-        if self.update_action_flag:
-            return self.update_action_flag
-        raise Exception('Please provide a `partial_update_action_flag` or '
-                        '`update_action_flag`.')
+        try:
+            return self.get_update_action_flag()
+        except Exception:
+            raise Exception('Please provide a `partial_update_action_flag` or '
+                            '`update_action_flag`.')
 
     def partial_update(self, request, *args, **kwargs):
         response = super().partial_update(request, *args, **kwargs)
@@ -75,6 +102,13 @@ class APILogViewSetMixin:
     def get_destroy_action_flag(self):
         if self.destroy_action_flag:
             return self.destroy_action_flag
+        obj_model = self.get_obj_model(self.get_object()).model
+        if obj_model in ENV_VAR_MODEL_LIST:
+            from .models import ENV_VAR_DELETION
+            return ENV_VAR_DELETION
+        elif obj_model in BUILD_OPTIONS_MODEL_LIST:
+            from .models import BUILD_OPTIONS_DELETION
+            return BUILD_OPTIONS_DELETION
         raise Exception('Please provide a `destroy_action_flag`.')
 
     def destroy(self, request, *args, **kwargs):
@@ -87,10 +121,15 @@ class APILogViewSetMixin:
             )
         return response
 
+    @classmethod
+    def obj_is_dapp(cls, obj):
+        obj_model = cls.get_obj_model(obj).model
+        return obj_model in DAPP_MODELS_LIST
+
     def get_dapp_object(self, obj, dapp=None):
         if dapp:
             return dapp
-        elif self.object_is_dapp:
+        elif self.obj_is_dapp(obj):
             return obj
         elif hasattr(self, 'get_dapp'):
             return self.get_dapp()
@@ -99,13 +138,13 @@ class APILogViewSetMixin:
             'dapp and have the `object_is_dapp` parameter set to True.')
 
     @classmethod
-    def get_obj_content_type(cls, obj):
+    def get_obj_model(cls, obj):
         return ContentType.objects.get_for_model(obj)
 
     def log_action(self, request, obj, action_flag, dapp=None):
         return APILog.objects.create(
             user=request.user,
-            content_type=self.get_obj_content_type(obj),
+            content_type=self.get_obj_model(obj),
             object_id=obj.pk,
             action_flag=action_flag,
             dapp=self.get_dapp_object(obj, dapp),
