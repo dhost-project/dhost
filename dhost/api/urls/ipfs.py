1413c4f41b06bb20171edd960b28e8e8dd19e147
2O4 <35725720+2O4@users.noreply.github.com>
2021-07-07 16:33:18 +0200

    add githuboptions

--- dhost/api/urls/ipfs.py
+++ dhost/api/urls/ipfs.py
@@ -13,28 +13,34 @@ router.register('', IPFSDappViewSet, basename='ipfs_dapp')
 ipfs_router = routers.SimpleRouter()
 ipfs_router.register('bundles', IPFSDappBundleViewSet)
 ipfs_router.register('deployments', IPFSDeploymentViewSet)
-ipfs_router.register('githuboptions', IPFSDappGithubOptionsViewSet)
 ipfs_router.register('logs', IPFSDappAPILogViewSet)
 
 ipfs_build_router = routers.SimpleRouter()
 ipfs_build_router.register('builds', IPFSDappBuildViewSet)
 ipfs_build_router.register('envvars', IPFSDappEnvVarViewSet)
 
-nested_urls = [
-    path('', include(ipfs_router.urls)),
+urlpatterns = [
+    path('', include(router.urls)),
+    path('<slug:dapp_slug>/', include(ipfs_router.urls)),
     path(
-        'buildoptions/',
+        '<slug:dapp_slug>/buildoptions/',
         IPFSDappBuildOptionsViewSet.as_view({
             'get': 'retrieve',
             'post': 'create',
             'put': 'update',
             'patch': 'partial_update',
             'delete': 'destroy'
-        })),
-    path('buildoptions/', include(ipfs_build_router.urls)),
-]
-
-urlpatterns = [
-    path('', include(router.urls)),
-    path('<slug:dapp_slug>/', include(nested_urls)),
+        }),
+    ),
+    path('<slug:dapp_slug>/buildoptions/', include(ipfs_build_router.urls)),
+    path(
+        '<slug:dapp_slug>/githuboptions/',
+        IPFSDappGithubOptionsViewSet.as_view({
+            'get': 'retrieve',
+            'post': 'create',
+            'put': 'update',
+            'patch': 'partial_update',
+            'delete': 'destroy'
+        }),
+    ),
 ]
