68ba64612d88e8dc50efb15350436b9a4d53176a
2O4 <35725720+2O4@users.noreply.github.com>
2021-07-11 13:39:56 +0200

    add google cloud storage support

--- dhost/core/storages.py
+++ dhost/core/storages.py
@@ -1,7 +1,12 @@
 from django.conf import settings
+from storages.backends.gcloud import GoogleCloudStorage
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class S3MediaStorage(S3Boto3Storage):
+class S3Boto3MediaStorage(S3Boto3Storage):
     bucket_name = settings.AWS_MEDIA_BUCKET_NAME
     custom_domain = settings.AWS_MEDIA_CUSTOM_DOMAIN
+
+
+class GoogleCloudMediaStorage(GoogleCloudStorage):
+    bucket_name = settings.GS_MEDIA_BUCKET_NAME
--- dhost/settings/defaults.py
+++ dhost/settings/defaults.py
@@ -145,8 +145,6 @@ STATIC_ROOT = os.path.join(ROOT_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 MEDIA_URL = env('MEDIA_URL', '/media/')
 
 MEDIA_ROOT = env('MEDIA_ROOT', os.path.join(ROOT_DIR, 'media'))
