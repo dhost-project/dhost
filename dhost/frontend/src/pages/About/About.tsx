b5a2fba08eaf627736ade8193bc59d1dc6b2d72f
2O4 <35725720+2O4@users.noreply.github.com>
2021-07-21 17:57:35 +0200

    add dapp list page

--- dhost/frontend/src/pages/About/About.tsx
+++ dhost/frontend/src/pages/About/About.tsx
@@ -1,12 +1,13 @@
+import Container from "react-bootstrap/Container"
 import { useTranslation } from "react-i18next"
 
 function About(): React.ReactElement {
   const { t } = useTranslation()
 
   return (
-    <div>
+    <Container>
       <h2>{t("ABOUT_TITLE")}</h2>
-    </div>
+    </Container>
   )
 }
 
