410065a6cf19e3d037e50a022848abef1ae849bb
2O4 <35725720+2O4@users.noreply.github.com>
2021-07-22 14:55:37 +0200

    add robots.txt to root url

--- dhost/urls.py
+++ dhost/urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -8,6 +9,13 @@ urlpatterns = [
     path("oauth2/", include("dhost.oauth2.urls", namespace="oauth2_provider")),
     path("social/", include("social_django.urls", namespace="social")),
     path("u/", include("dhost.users.urls")),
+    path(
+        "robots.txt",
+        TemplateView.as_view(
+            template_name="robots.txt",
+            content_type="text/plain",
+        ),
+    ),
 ]
 
 admin.site.site_header = "DHost"
