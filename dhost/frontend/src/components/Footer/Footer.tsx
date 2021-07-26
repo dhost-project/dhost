83c7ef2eed7f67497cd155faeb4bf02abf1896b9
2O4 <35725720+2O4@users.noreply.github.com>
2021-07-26 21:44:46 +0200

    minimalist footer

--- dhost/frontend/src/components/Footer/Footer.tsx
+++ dhost/frontend/src/components/Footer/Footer.tsx
@@ -5,11 +5,39 @@ import "./style.scss"
 function Footer(): React.ReactElement {
   return (
     <footer>
-      <div id="footer-nav">
-        <Container></Container>
-      </div>
-      <div id="footer-bottom">
-        <Container className="py-1">DHost 2020-2021.</Container>
+      <div className="d-flex border-top bg-light fw-light py-2 px-2">
+        <div className="me-auto">
+          <a href="https://github.com/dhost-project/dhost-doc" target="_blank">
+            Documentation
+          </a>
+          <a
+            href="https://github.com/dhost-project/dhost-examples"
+            target="_blank"
+          >
+            Examples
+          </a>
+          <a href="https://github.com/dhost-project/dhost" target="_blank">
+            Source code
+          </a>
+          <a
+            href="https://github.com/dhost-project/dhost/issues/new"
+            target="_blank"
+          >
+            Issues
+          </a>
+          <a href="">Support</a>
+        </div>
+        <div>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+            Privacy
+          </a>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+            Legal
+          </a>
+          <a href="/">
+            <span className="text-primary">D</span>Host 2020-2021
+          </a>
+        </div>
       </div>
     </footer>
   )
