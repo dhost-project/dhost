b26f5a52ea50de2dc8a7f3b167d879c96df1051d
2O4 <35725720+2O4@users.noreply.github.com>
2021-07-26 17:08:34 +0200

    added footer

--- dhost/frontend/src/App.tsx
+++ dhost/frontend/src/App.tsx
@@ -1,7 +1,21 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+
+import "./App.css"
 import RouterOutlet from "./Router"
 
 function App(): React.ReactElement {
-  return <RouterOutlet />
+  return (
+    <div id="footer-flex-wrapper">
+      <div>
+        <Header />
+        <Navbar />
+        <RouterOutlet />
+      </div>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
--- dhost/frontend/src/components/Footer/Footer.tsx
+++ dhost/frontend/src/components/Footer/Footer.tsx
@@ -1,5 +1,18 @@
+import Container from "react-bootstrap/Container"
+
+import "./style.scss"
+
 function Footer(): React.ReactElement {
-  return <div></div>
+  return (
+    <footer>
+      <div id="footer-nav">
+        <Container></Container>
+      </div>
+      <div id="footer-bottom">
+        <Container className="py-1">DHost 2020-2021.</Container>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
