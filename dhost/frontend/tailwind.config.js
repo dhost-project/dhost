047ae13559f1efa1dd8ff05b3f13104ffb6f94b9
2O4 <35725720+2O4@users.noreply.github.com>
2021-07-28 14:02:17 +0200

    add primary and secondary colors

--- dhost/frontend/tailwind.config.js
+++ dhost/frontend/tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#cfff95',
+          DEFAULT: '#9ccc65',
+          dark: '#6b9b37',
+        },
+        secondary: {
+          light: '#fffffb',
+          DEFAULT: '#d7ccc8',
+          dark: '#a69b97',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
