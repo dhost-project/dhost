db3593b97d40dabc02287dc8b3871b87a18890f8
2O4 <35725720+2O4@users.noreply.github.com>
2021-08-01 12:15:17 +0200

    style prettier

--- dhost/frontend/src/components/Dappbar/NavItem.tsx
+++ dhost/frontend/src/components/Dappbar/NavItem.tsx
@@ -12,7 +12,9 @@ export default function NavItem({
   return (
     <a
       href={href}
-      className={`dappbar-item relative px-3 py-2 text-gray-700 ${isActive ? "active" : "hover:text-gray-900"}`}
+      className={`dappbar-item relative px-3 py-2 text-gray-700 ${
+        isActive ? "active" : "hover:text-gray-900"
+      }`}
     >
       {children}
     </a>
