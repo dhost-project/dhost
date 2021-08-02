89a43d6759879467abf9ee1e13629cb7eca3cd96
2O4 <35725720+2O4@users.noreply.github.com>
2021-08-02 16:42:56 +0200

    style navbar search

--- dhost/frontend/src/components/Navbar/Navbar.tsx
+++ dhost/frontend/src/components/Navbar/Navbar.tsx
@@ -80,7 +80,7 @@ export default function Navbar(): React.ReactElement {
             </div>
             <div className="hidden md:flex md:flex-1 px-16">
               <input type="text" className="flex-grow px-2 rounded-l border" placeholder="Search dapps" />
-              <button className="flex-none px-2 rounded-r border-r border-b border-t"><SearchIcon className="h-5" /></button>
+              <button className="flex-none px-2 text-gray-500 rounded-r border-r border-b border-t"><SearchIcon className="h-5" /></button>
             </div>
             <div className="hidden md:flex justify-end md:flex-1 lg:w-0">
               <div>
