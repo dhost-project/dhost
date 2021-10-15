e50dcce6dbd91d356586668b6c730e33c6dc2f88
2O4 <35725720+2O4@users.noreply.github.com>
2021-10-15 10:26:50 +0200

    add tasks

--- dhost/ipfs/tasks.py
+++ dhost/ipfs/tasks.py
@@ -1,6 +1,12 @@
+from dhost.ipfs.ipfs import IPFSAPI
+
+ipfs = IPFSAPI()
+
+
 def task_add_file(file_path):
-    pass
+    ipfs_obj = ipfs.add("file_path")
+    ipfs.pin_add(arg=ipfs_obj["Hash"])
 
 
-def task_remove_file(file_path):
-    pass
+def task_remove_file(hash):
+    ipfs.pin_rm(hash)
