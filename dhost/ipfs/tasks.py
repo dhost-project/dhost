b83dcd0cf43c888c6238070920b693fd16eb2ad8
2O4 <35725720+2O4@users.noreply.github.com>
2021-10-15 10:39:47 +0200

    fix tasks

--- dhost/ipfs/tasks.py
+++ dhost/ipfs/tasks.py
@@ -1,12 +1,15 @@
+from celery import shared_task
 from dhost.ipfs.ipfs import IPFSAPI
 
 ipfs = IPFSAPI()
 
 
+@shared_task
 def task_add_file(file_path):
-    ipfs_obj = ipfs.add("file_path")
+    ipfs_obj = ipfs.add(file_path)
     ipfs.pin_add(arg=ipfs_obj["Hash"])
 
 
+@shared_task
 def task_remove_file(hash):
     ipfs.pin_rm(hash)
