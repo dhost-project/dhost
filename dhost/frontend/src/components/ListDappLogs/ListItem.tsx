9949ccdff0f10b1d0f9883460236d5a86e354d2c
NicolasPandraud <nicolas.pandraud@riskee.io>
2022-04-01 14:20:09 +0200

    Update logs

--- dhost/frontend/src/components/ListDappLogs/ListItem.tsx
+++ dhost/frontend/src/components/ListDappLogs/ListItem.tsx
@@ -1,39 +1,49 @@
-import { DappLogs } from "models/api/DappLogs"
 import { useEffect } from "react"
 import { useTranslation } from "react-i18next"
-
+import { DappLogs } from "models/api/DappLogs"
 
 export function ListItem({
-    dappLogs,
+  dappLog,
+  gap,
 }: {
-    dappLogs: DappLogs,
+  dappLog: DappLogs
+  gap: boolean
 }): React.ReactElement {
-    const { t } = useTranslation()
-    var colors;
+  const { t } = useTranslation()
+  var colors
 
-    switch (true) {
-        case /success/.test(dappLogs.action_flag):
-            colors =
-                "bg-green-100 text-green-600 hover:bg-green-200 hover:text-green-700"
-            break;
-        case /error/.test(dappLogs.action_flag):
-            colors = "bg-red-100 text-red-600 hover:bg-red-200 hover:text-red-700"
-            break;
-        default:
-            colors = "bg-gray-100 text-gray-600 hover:bg-gray-200 hover:text-gray-700"
-            break;
-    }
+  let date = new Date(dappLog.action_time)
 
-    return (
-        <a
-            className={`${colors} px-4 py-2`}
-            href={`/dappLogs/${dappLogs.id}/`}
-        >
-            {/* {dappLogs.user} */}
-            {t(dappLogs.action_flag)}
-            {dappLogs.change_message}
-            <br />
-            {new Date(dappLogs.action_time).toLocaleDateString()}
-        </a>
-    )
+  switch (true) {
+    case /success|dapp_add/.test(dappLog.action_flag):
+      colors =
+        "bg-green-100 text-green-600 hover:bg-green-200 hover:text-green-700"
+      break
+    case /error/.test(dappLog.action_flag):
+      colors = "bg-red-100 text-red-600 hover:bg-red-200 hover:text-red-700"
+      break
+    default:
+      colors = "bg-gray-100 text-gray-600 hover:bg-gray-200 hover:text-gray-700"
+      break
+  }
+
+  return (
+    <a
+      className={`${colors} px-4 py-2`}
+      href={`/dappLogs/${dappLog.id}/`}
+      style={
+        gap
+          ? { marginBottom: "5px", border: "none", borderRadius: "5px" }
+          : { marginBottom: "0" }
+      }
+    >
+      {/* {dappLog.user} */}
+      {t(dappLog.action_flag)}
+      {dappLog.change_message}
+      <br />
+      <p className="italic text-sm" style={{ color: "grey" }}>
+        {date.toLocaleString()}
+      </p>
+    </a>
+  )
 }
--- dhost/frontend/src/pages/Dapp/DappLogs/DappLogsList.tsx
+++ dhost/frontend/src/pages/Dapp/DappLogs/DappLogsList.tsx
@@ -1,20 +1,14 @@
-import { SearchIcon } from "@heroicons/react/outline"
-import { useTranslation } from "react-i18next"
-import { Button } from "components/Button"
 import { ListDappLogs } from "components/ListDappLogs"
 import { DappLogs } from "models/api/DappLogs"
-import { useEffect } from "react"
 
 export function DappLogsList({
-    dappLogs,
+  dappLogs,
 }: {
-    dappLogs: DappLogs[]
+  dappLogs: DappLogs[]
 }): React.ReactElement {
-    const { t } = useTranslation()
-
-    return (
-        <div className="container mx-auto py-4">
-            <ListDappLogs dappLogsList={dappLogs} viewAllLogs={true} />
-        </div>
-    )
+  return (
+    <div className="container mx-auto my-4">
+      <ListDappLogs dappLogsList={dappLogs} logsCount={10} gap={false} />
+    </div>
+  )
 }
