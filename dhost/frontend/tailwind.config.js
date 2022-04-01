edc8f5395c7d0a82fa6623d01f6a448968587226
NicolasPandraud <nicolas.pandraud@riskee.io>
2022-04-01 14:21:40 +0200

    Update tailwindConfig

--- dhost/frontend/tailwind.config.js
+++ dhost/frontend/tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   mode: "jit",
   purge: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/*.{js,jsx,ts,tsx}",
+    "./src/**/**/**/*.{js,jsx,ts,tsx}",
     "./public/index.html",
   ],
   darkMode: false, // or 'media' or 'class'
