b49620ed88e8daa9415f98228d6715fae6aa7382
elizabethsoto26 <elizabeth.soto@epitech.eu>
2022-04-02 19:05:16 +0200

    Update signals Unpin CID and call garbage collector when deleting IPFSDapp and Delete bundle folder when deleting Bundle object

--- dhost/ipfs/apps.py
+++ dhost/ipfs/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class IPFSConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "dhost.ipfs"
+    def ready(self):
+        import dhost.ipfs.signals  # noqa5.
